e7c5944bbb1b4b1861839e434e2a641fadc03c18
Vinayak <svinayak.30@gmail.com>
2024-12-31 20:10:51 +0530

    codeRefactor

---
+++ src/main/java/usermgmt_ms/usermgmt_ms/UsermgmtMsApplication.java
@@ -0,0 +1,13 @@
+package usermgmt_ms.usermgmt_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsermgmtMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsermgmtMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/usermgmt_ms/usermgmt_ms/UsermgmtMsApplicationTests.java
@@ -0,0 +1,13 @@
+package usermgmt_ms.usermgmt_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsermgmtMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
